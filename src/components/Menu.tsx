1e0278ae0a10ea950a1165da7dc4ce9b2d71391c
danial_ab <danialabubakar91@gmail.com>
2024-07-01 12:36:34 +0800

    Added Header with menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+
+const Menu:React.FC = () => {
+    return (
+        <div className="md:flex">
+          <h1 className="border-t border-gray-600">Home</h1>
+          <h1 className="border-t border-gray-600">Projects</h1>
+          <h1 className="border-y border-gray-600">Resume</h1>
+        </div>
+      );
+}
+
+export default Menu
