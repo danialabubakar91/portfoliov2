fa00d83fad50192542b7ec25f1ceae4ab7f01991
danial_ab <danialabubakar91@gmail.com>
2024-07-01 10:41:32 +0800

    Setup react router including route pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Layout from './components/Layout'
+import Home from './pages/Home'
+import Projects from "./pages/Projects"
+
 const App:React.FC = () => {
   return(
-    <Layout/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
