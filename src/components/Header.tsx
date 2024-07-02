fcc78aebca45f7e330a0babb2e1d904aa79e2f6d
danial_ab <danial_ab@nec.com.sg>
2024-07-02 18:39:55 +0800

    Added menu item links in dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,14 +9,20 @@ const Header: React.FC = () => {
       <Menu>
         <div className="flex justify-between pt-5">
           <div className="ml-10p">DANIAL</div>
-          <MenuButton className={"mr-10p md:hidden"}/>
+          <MenuButton className={"mr-10p md:hidden"} />
           <nav className="mr-10p hidden md:block">
             <MenuItem path={"/"}>Home</MenuItem>
             <MenuItem path={"projects"}>Projects</MenuItem>
             <MenuItem path={"projects"}>Resume</MenuItem>
           </nav>
         </div>
-        <MenuDropdown />
+        <MenuDropdown>
+          <div className="md:hidden">
+            <MenuItem path={"/"}>Home</MenuItem>
+            <MenuItem path={"projects"}>Projects</MenuItem>
+            <MenuItem path={"projects"}>Resume</MenuItem>
+          </div>
+        </MenuDropdown>
       </Menu>
     </header>
   );
