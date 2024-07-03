df6f6fa62f92011f0c5ff8ed8b2c950d5e430dc7
danial_ab <danial_ab@nec.com.sg>
2024-07-03 19:12:12 +0800

    Fixed radix issue

--- src/components/Menu/MenuRadix.tsx
+++ src/components/Menu/MenuRadix.tsx
@@ -1,23 +1,51 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 interface Props {
   className: string;
 }
 
 const MenuRadix: React.FC<Props> = ({ className }: Props) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenChange = (open: boolean) => {
+    console.log('something happened here')
+    setIsOpen(open);
+  };
+
+  const handleLinkClick = () => {
+    console.log('something happened there')
+    setIsOpen(false);
+  };
+
   return (
     <div className={className}>
-      <DropdownMenu.Root>
+      <DropdownMenu.Root open={isOpen} onOpenChange={handleOpenChange}>
         <DropdownMenu.Trigger asChild>
           <button>radixbutton</button>
         </DropdownMenu.Trigger>
-
         <DropdownMenu.Portal>
-            <DropdownMenu.Content className="bg-white border border-indigo-600 w-screen flex flex-col items-center">
-              <DropdownMenu.Item className="border border-pink-600 w-4/5 text-end">radix home</DropdownMenu.Item>
-              <DropdownMenu.Item className="border border-pink-600 w-4/5 text-end">radix projects</DropdownMenu.Item>
-              <DropdownMenu.Item className="border border-pink-600 w-4/5 text-end">radix resume</DropdownMenu.Item>
-            </DropdownMenu.Content>
+          <DropdownMenu.Content
+            className={`bg-white border border-indigo-600 w-screen flex flex-col items-center`}
+          >
+            <DropdownMenu.Item className="border border-pink-600 w-full text-end">
+              <Link to="/" className="block" onClick={handleLinkClick}>
+                Home<span className="pr-[10vw]"></span>
+              </Link>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className="border border-pink-600 w-full text-end">
+              <Link to="projects" className="block">
+                Projects<span className="pr-[10vw]"></span>
+              </Link>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className="border border-pink-600 w-full text-end">
+              <Link to="projects" className="block">
+                Resume<span className="pr-[10vw]"></span>
+              </Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
         </DropdownMenu.Portal>
       </DropdownMenu.Root>
     </div>
